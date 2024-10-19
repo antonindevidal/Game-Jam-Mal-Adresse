0d2d0e43a94928cb1df839c2a7e8bba595e8dcd9
antonindevidal <devidalantonin@gmail.com>
2024-10-19 19:45:11 +0200

    Main menu working

--- Scripts/MainMenu/Level.gd
+++ Scripts/MainMenu/Level.gd
@@ -1,17 +1,24 @@
 extends StaticBody3D
 
 signal train_destination(new_destination); 
+signal level_selected(level_number)
 
-# Called when the node enters the scene tree for the first time.
+@export var levelNumber : int = 0
+var is_mouse_hover : bool = false
+
 func _ready() -> void:
-	connect("mouse_entered", onMouseEntered)
+	connect("mouse_entered", _on_mouse_entered)
+	connect("mouse_exited", _on_mouse_exited)
 	pass # Replace with function body.
 
-func onMouseEntered() -> void : 
-	print("mouse entered", position)
+func _on_mouse_entered() -> void : 
+	is_mouse_hover = true
 	emit_signal("train_destination", position)
 	
-	
+func _on_mouse_exited() -> void : 
+	is_mouse_hover = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("LeftClick") and is_mouse_hover == true:
+		emit_signal("level_selected",levelNumber)
