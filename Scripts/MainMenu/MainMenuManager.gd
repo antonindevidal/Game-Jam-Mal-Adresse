c0b1a1f2199ebbd14bcad5a61da002eb99a33d28
HolyBlue <hugobarrandon@gmail.com>
2024-10-20 15:08:12 +0200

    Add level in main

--- Scripts/MainMenu/MainMenuManager.gd
+++ Scripts/MainMenu/MainMenuManager.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+var lvl1 : PackedScene = preload("res://Scenes/Levels/Level1.tscn")
+var lvl2 : PackedScene = preload("res://Scenes/Levels/Level2.tscn")
+var lvl3 : PackedScene = preload("res://Scenes/Levels/Level3.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,9 +19,12 @@ func _on_level_selected(level_number: Variant) -> void:
 	match level_number:
 		1:
 			#change scene to level one
+			get_tree().change_scene_to_packed(lvl1)
 			pass
 		2:
+			get_tree().change_scene_to_packed(lvl2)
 			pass
 		3:
+			get_tree().change_scene_to_packed(lvl3)
 			pass
 	pass # Replace with function body.
